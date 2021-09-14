ec8eb6ffc43ecb9a4ee8853d7efedf2d24a4de33
Angelika Jungco <angelika.jungco@usjr.edu.ph>
2021-09-14 15:58:10 +0800

    Auto stash before rebase of "refs/heads/main"

--- CalculatorInteractive/Calculator.js
+++ CalculatorInteractive/Calculator.js
@@ -9,20 +9,58 @@ var answer = document.getElementById("answer");
 plus.addEventListener("click", function () {
     var ans = parseInt(num1.value) + parseInt(num2.value);   
     answer.innerText = ans;
+
+    plus.style.background = "rgb(0, 204, 255)";
+    subtract.style.background = "rgb(90, 90, 90)";
+    multiply.style.background = "rgb(90, 90, 90)";
+    divide.style.background = "rgb(90, 90, 90)";
 })
 
 subtract.addEventListener("click", function () {
     var ans = parseInt(num1.value) - parseInt(num2.value);
     answer.innerText = ans;
+
+    plus.style.background = "rgb(90, 90, 90)";
+    subtract.style.background = "red";
+    multiply.style.background = "rgb(90, 90, 90)";
+    divide.style.background = "rgb(90, 90, 90)";
 })
 
 multiply.addEventListener("click", function () {
     var ans = parseInt(num1.value) * parseInt(num2.value);
     answer.innerText = ans;
+
+    plus.style.background = "rgb(90, 90, 90)";
+    subtract.style.background = "rgb(90, 90, 90)";
+    multiply.style.background = "green";
+    divide.style.background = "rgb(90, 90, 90)";
 })
 
 divide.addEventListener("click", function () {
     var ans = parseInt(num1.value) / parseInt(num2.value);
     answer.innerText = ans;
+
+    plus.style.background = "rgb(90, 90, 90)";
+    subtract.style.background = "rgb(90, 90, 90)";
+    multiply.style.background = "rgb(90, 90, 90)";
+    divide.style.background = "rgb(68, 0, 255)";
 })
 
+document.addEventListener("DOMContentLoaded", function(event) {
+    document.documentElement.setAttribute("data-theme", "light");
+    
+    // Get our button switcher
+    var themeSwitcher = document.getElementById("customSwitches");
+
+    // When our button gets clicked
+    themeSwitcher.onclick = function() {
+      // Get the current selected theme, on the first run
+      // it should be `light`
+      var currentTheme = document.documentElement.getAttribute("data-theme");
+      // Switch between `dark` and `light`
+      var switchToTheme = currentTheme === "dark" ? "light" : "dark"
+
+      // Set our currenet theme to the new one
+      document.documentElement.setAttribute("data-theme", switchToTheme);
+    }
+  });
