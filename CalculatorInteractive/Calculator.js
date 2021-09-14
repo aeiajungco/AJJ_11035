5a3a922e0def129959276f97283d931192283987
aeiajungco <90016886+aeiajungco@users.noreply.github.com>
2021-09-14 16:15:39 +0800

    Update Calculator.js

--- CalculatorInteractive/Calculator.js
+++ CalculatorInteractive/Calculator.js
@@ -49,18 +49,12 @@ divide.addEventListener("click", function () {
 document.addEventListener("DOMContentLoaded", function(event) {
     document.documentElement.setAttribute("data-theme", "light");
     
-    // Get our button switcher
     var themeSwitcher = document.getElementById("customSwitches");
 
-    // When our button gets clicked
     themeSwitcher.onclick = function() {
-      // Get the current selected theme, on the first run
-      // it should be `light`
       var currentTheme = document.documentElement.getAttribute("data-theme");
-      // Switch between `dark` and `light`
       var switchToTheme = currentTheme === "dark" ? "light" : "dark"
-
-      // Set our currenet theme to the new one
+      
       document.documentElement.setAttribute("data-theme", switchToTheme);
     }
   });
